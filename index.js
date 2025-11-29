dee56c9c771841455ae885cc8a6a9b1fe0fd61bb
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-11-29 10:15:36 +0900

    minimum release

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.log("This is ycookiey!");
+console.log("--------------------------");
+console.log("Web: https://ycookiey.com");
+console.log("GitHub: https://github.com/ycookiey");
+console.log("--------------------------");
